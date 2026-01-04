d3e9b553bfe15281d0752ba8e5221b78b54fec56
Davit Abramishvili🔸 <david.abramishvili10@gmail.com>
2026-01-05 03:49:14 +0400

    chore: fixed monorepo structure

--- gym_crm/src/main/java/com/example/gymcrm/config/WebConfig.java
+++ gym_crm/src/main/java/com/example/gymcrm/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.gymcrm.web;
+package com.example.gymcrm.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
